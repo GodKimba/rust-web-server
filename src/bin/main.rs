8a74acfd937f04a54e18ac9f98ebf2a4e1458f9e
Rafael Fadel Silva <rafaelfadel2@gmail.com>
2022-07-16 21:04:25 -0300

    started to create library to handle multithread requests

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,17 @@
-use std::fs;
+use single_threaded_web_server::ThreadPool;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::{fs, thread};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
